a18ba7c6831b4423ca631a105d8a11a58cdbc20f
mvolpe11 <mikea.volpe@gmail.com>
2017-06-14 18:59:15 -0400

    Update config.js

--- recipes/speech_to_text/config.js
+++ recipes/speech_to_text/config.js
@@ -4,6 +4,6 @@ exports.credentials = {};
 // Watson Speech to Text
 // https://www.ibm.com/watson/developercloud/speech-to-text.html
 exports.credentials.speech_to_text = {
-    password: 'muNvCnZZNWx7',
-    username: '92f9a714-77cf-4da5-b161-1ef7b4b63935'
+	password: 'muNvCnZZNWx7',
+	username: '92f9a714-77cf-4da5-b161-1ef7b4b63935'
 };
